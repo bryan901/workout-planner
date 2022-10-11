84703ca49066e8c42413574ab8751e8d0be9c937
alexanderyoza <alex.yoza@gmail.com>
2022-10-11 12:25:05 -0700

    single componenet

--- react/src/App.js
+++ react/src/App.js
@@ -1,15 +1,38 @@
 import './App.css';
-import Header from './Components/Header';
-import Sport from './Components/Sport';
-import Days from './Components/Days';
 
 function App() {
 
   return (
     <div>
-      <Header/>
-      <Sport/>
-      <Days/>
+      <div className='header'>
+        <div className='title-text'>{title}</div>
+      </div>
+      <div className='header'>
+            <div className='title-text'>
+                <form action='#'>
+                    <label htmlFor='sport'>{prompt}</label>
+                    <select name='sport' id='sport'>
+                        <option value='soccer'>Soccer</option>
+                        <option value='volleyball'>Volleyball</option>
+                        <option value='football'>Football</option>
+                        <option value='basketball'>Basketball</option>
+                    </select>
+
+                    <label htmlFor='days'>{prompt}</label>
+                    <select name='days' id='days'>
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                        <option value='5'>5</option>
+                        <option value='6'>6</option>
+                        <option value='7'>7</option>
+                    </select>
+
+                    <input type='submit' value='Submit'/>
+                </form>
+            </div>
+        </div>
     </div>
   );
 }
